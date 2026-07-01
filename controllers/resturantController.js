a88b24c1bf4381e2d87030eccd40d6d9907ca22e
Arya Nath <nath.arya2000@gmail.com>
2026-07-01 14:14:20 +0530

    Initial commit

---
+++ controllers/resturantController.js
@@ -0,0 +1,105 @@
+const resturantModel = require("../models/resturantModel");
+const createResturantController = async (req, res) => {
+  try {
+    const {
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    } = req.body;
+    if (!title || !coords) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Please provide title and address." });
+    }
+    const newResturant = new resturantModel({
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    });
+    await newResturant.save();
+    res.status(201).send({
+      success: true,
+      message: "New Resturant Created Successfully.",
+      newResturant,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Create Resturant API",
+      error,
+    });
+  }
+};
+
+const GetAllResturantController = async (req, res) => {
+  try {
+    const resturant = await resturantModel.find();
+    if (!resturant.length) {
+      return res.status(404).send({
+        success: false,
+        message: "No Resturant  Found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Successfully Found the Resturant.",
+      resturant,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in get all API",
+      error,
+    });
+  }
+};
+
+const GetResturantController = async (req, res) => {
+  try {
+    const resturant = await resturantModel.findById(req.params.id);
+    if (!resturant) {
+      res.status(404).send({
+        success: false,
+        message: "Resturant not found.",
+      });
+    }
+    res.status(201).send({
+      success: true,
+      message: "Successfully Found the Resturant",
+      resturant,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Get Restutant API",
+    });
+  }
+};
+
+module.exports = {
+  createResturantController,
+  GetAllResturantController,
+  GetResturantController,
+};
---
+++ routes/resturantRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const authMiddleware = require("../middleware/authMiddleware");
+const {
+  createResturantController,
+  GetAllResturantController,
+  GetResturantController,
+} = require("../controllers/resturantController");
+
+const router = express.Router();
+
+// routes
+// Create Resturant || PUSH
+
+router.post("/create", authMiddleware, createResturantController);
+
+// Get all Resturant || GET
+router.get("/getAll", authMiddleware, GetAllResturantController);
+
+// Get  A Resturant
+
+router.get("/get/:id", authMiddleware, GetResturantController);
+
+module.exports = router;

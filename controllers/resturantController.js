9a27a2e141aaa3faa7324316e01aaeef7889d3e7
Arya Nath <nath.arya2000@gmail.com>
2026-07-01 16:48:20 +0530

    fix: improve error handling in DeleteResturantController

--- controllers/resturantController.js
+++ controllers/resturantController.js
@@ -115,6 +115,11 @@ const DeleteResturantController = async (req, res) => {
     }
   } catch (error) {
     console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Delete Resturant API",
+      error,
+    });
   }
 };
 
